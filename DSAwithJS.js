e5fa41253bbe5da57a1ad4515d48b46e966775a9
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-05-29 10:44:17 +0500

    different method used for swapping

---
+++ DSAwithJS.js
@@ -0,0 +1,42 @@
+// const a = "6";
+// const b = 4;
+
+// console.log(a - b) // 2
+// console.log(a + b) // 64
+// console.log(a * b) // 24
+// console.log(a / b) // 2.5
+// console.log(Number(a) + b) // 10 ->TypeConversion
+
+
+// console.log(Number("12")) // 12
+// console.log(Number(12)) // 12
+// console.log(Number("HMK")) // NaN
+// console.log(typeof Number("HMK")) // number
+// console.log(Number(false)) // 0
+// console.log(Number(true)) // 1
+
+// Swapping
+
+// Method 1 with extra variable
+// let a = 10;
+// let b = 20;
+// let c = a;
+// a = b;
+// b = c;
+// console.log(a, b, c) // 20,10,10
+
+// Method 2 without third variable
+// let a = 59;
+// let b = 32;
+// a = a + b; // a = 91
+// b = a - b; // b = 59
+// a = a - b; // a = 32;
+// console.log(a, b) // 32 59
+
+// Method 3 Destructure
+// let a = 88;
+// let b = 43;
+// [a, b] = [b, a]
+// console.log(a, b)
+
+// Operators 
