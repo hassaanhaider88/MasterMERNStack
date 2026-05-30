29b921b5ae35b0831d916687a25bbad5c7b6c165
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-05-30 11:48:20 +0500

    sorting negative and positive Element Work

--- DSAwithJS.js
+++ DSAwithJS.js
@@ -446,4 +446,17 @@
 // }
 // console.log(arr)
 
-// to be contiue inshallah 
+// sorting negative and positive Element
+// let arr = [1, 2, -4, 2, -2, 6, -9, 3, -5]
+// let i = 0;
+// let j = 0;
+// while (i < arr.length) {
+//     if (arr[i] < 0) {
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         j++
+//     }
+//     i++
+// }
+// console.log(arr)
