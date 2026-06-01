7a86537c5ba4e6a1e9444ceb4744b03b05a604d2
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-06-01 10:48:20 +0500

    Merge Sort

--- DSAwithJS.js
+++ DSAwithJS.js
@@ -786,4 +786,35 @@
 // let index = binarySearch(arr, 0, arr.length - 1, 6);
 // console.log(index)
 
-// Merge Sort (Divide & Merge)
+// // Merge Sort (Divide & Merge)
+// function Merge(arr, first, mid, last) {
+//     let temp = new Array(last - first + 1);
+//     let i = first, j = mid + 1, k = 0;
+//     while (i <= mid && j <= last) {
+//         if (arr[i] < arr[j]) {
+//             temp[k++] = arr[i++];
+//         } else temp[k++] = arr[j++]
+//     }
+//     while (i <= mid) {
+//         temp[k++] = arr[i++]
+//     }
+//     while (j <= last) {
+//         temp[k++] = arr[j++]
+//     }
+//     let p = 0, t = first;
+//     while (p < temp.length) {
+//         arr[t++] = temp[p++]
+//     }
+// }
+
+// function Divide(arr, first, last) {
+//     if (first >= last) return;
+//     let mid = Math.floor((first + last) / 2)
+//     Divide(arr, first, mid)
+//     Divide(arr, mid + 1, last)
+//     Merge(arr, first, mid, last)
+// }
+
+// let arr = [8, 2, 1, 9, 5, 12, 4, 20]
+// Divide(arr, 0, arr.length - 1)
+// console.log(arr)
